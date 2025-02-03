df239fd5fc290302a26f82e9d744e13225fea350
tazmeenilsa <tazmeenilsa@gmail.com>
2025-02-03 22:55:12 +0500

    user & video model and jwt token

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,5 +1,6 @@
 import mongoose, { Schema } from "mongoose";
 import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
 const userSchema = new mongoose.Schema(
     {
         username: {
@@ -51,18 +52,35 @@ const userSchema = new mongoose.Schema(
 
 // dont use arrow function i pre method it triggered some problems because it didn't get this
 userSchema.pre("save", async function (next) {
-
     if (!this.isModified('password')) return next()
     //bycrypt helps you to hash your password
-
     this.password = bcrypt.hash(this.password, 10)
     next()
 })
-
-
-
 userSchema.methods.isPasswordCorrect = async function (password) {
     return await bcrypt.compare(password, this.password) //its return true and false
 }
-
+userSchema.methods.generateAccessToken = function () {
+    jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username,
+            fullname: this.fullname
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        })
+}
+userSchema.methods.generateRefreshToken = function () {
+    jwt.sign(
+        {
+            _id: this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        })
+}
 export const User = mongoose.model("User", userSchema)
